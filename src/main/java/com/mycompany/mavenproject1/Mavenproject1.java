71254c4f93ed76ee698cf543b07e645f2f0e3356
David Aragundy <waldaara@espol.edu.ec>
2023-12-07 20:24:03 -0500

    first commit

---
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,12 @@
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Hp
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
